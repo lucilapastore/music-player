1c06c5f4d10dda1d364e75579545717c7c2a4ff2
lucilapastore <lucilapastore@gmail.com>
2025-03-31 19:50:41 -0300

    Implement song sorting by title in the sortSongs function

--- script.js
+++ script.js
@@ -102,6 +102,14 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 const sortSongs = () => {
-  userData?.songs.sort((a, b) => {});
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
 };
 renderSongs(userData?.songs);
